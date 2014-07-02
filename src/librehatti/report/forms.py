2b78766dc25b23cab0d822a6104c980727a75cef
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-02 23:43:21 +0530

    django form for the search interface

--- src/librehatti/report/forms.py
+++ src/librehatti/report/forms.py
@@ -1,27 +1,28 @@
 from django import forms
-#from search_choices import CLIENT_FIELD_CHOICES,CLIENT_ORDER_CHOICES, CLIENT_ORDER_TYPES_CHOICES
-#from search_choices import JOB_SEARCH_CHOICES
 from search_choices import *
 import datetime
 
-
+"""
+displays checkboxes for Client Search
+"""
 class ClientForm(forms.Form):
-        client_fields = forms.MultipleChoiceField(required=False,
-        widget=forms.CheckboxSelectMultiple, choices=CLIENT_FIELD_CHOICES)
-        
-       
-        
-        
+    client_fields = forms.MultipleChoiceField(required=False,
+    widget=forms.CheckboxSelectMultiple, choices=CLIENT_FIELD_CHOICES)
+
+"""
+displays chechboxes for Order Search
+"""        
 class OrderForm(forms.Form):
-        order = forms.MultipleChoiceField(required=False,
-        widget=forms.CheckboxSelectMultiple, choices=CLIENT_ORDER_CHOICES)
+    order = forms.MultipleChoiceField(required=False,
+    widget=forms.CheckboxSelectMultiple, choices=CLIENT_ORDER_CHOICES)
 
-
-        
+"""
+displays checkboxes for Constraints
+"""
 class AddConstraints(forms.Form):
-       
-        start_date = forms.DateField(required=False, initial='2014-01-01')
-        end_date = forms.DateField(required=False, initial= datetime.date.today())
-        additional_constraints =  forms.MultipleChoiceField(required=False,widget=forms.CheckboxSelectMultiple, choices= CONSTRAINT_CHOICES)
-        amount_greater_than = forms.FloatField(required=False, initial = 0)
-        amount_less_than = forms.FloatField(required=False, initial = 1000000)
+    start_date = forms.DateField(required=False, initial='2014-01-01')
+    end_date = forms.DateField(required=False, initial= datetime.date.today())
+    additional_constraints = forms.MultipleChoiceField(required=False,
+    widget=forms.CheckboxSelectMultiple, choices= CONSTRAINT_CHOICES)
+    amount_greater_than = forms.FloatField(required=False, initial = 0)
+    amount_less_than = forms.FloatField(required=False, initial = 1000000)
