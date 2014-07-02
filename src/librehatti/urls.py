e308f9eaa109a3fdd29d63704caeecf58a389f20
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-02 23:35:54 +0530

    urls.py including url for search and search_result function

--- src/librehatti/urls.py
+++ src/librehatti/urls.py
@@ -8,12 +8,10 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-        
-        url(r'^$', 'librehatti.catalog.views.index'),
-        url(r'^catalog/', include('librehatti.catalog.urls')),
-        url(r'^useraccounts/', include('useraccounts.urls')),
-        url(r'^admin/', include(admin.site.urls)),
-        url(r'^bill/','librehatti.report.views.bill'),
-        url(r'^index/','librehatti.report.views.index'),
-        url(r'^display/','librehatti.report.views.display'),
+    url(r'^$', 'librehatti.catalog.views.index'),
+    url(r'^catalog/', include('librehatti.catalog.urls')),
+    url(r'^useraccounts/', include('useraccounts.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^search/','librehatti.report.search.search'),
+    url(r'^search_result/','librehatti.report.views.search_result'),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
