61ce243b46f43f5c38b64a3a50a2d671a140d833
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-03 19:44:08 +0530

    comments done

--- src/librehatti/report/forms.py
+++ src/librehatti/report/forms.py
@@ -1,25 +1,25 @@
 from django import forms
-from search_choices import *
+from search_choices import CLIENT_FIELD_CHOICES, CLIENT_ORDER_CHOICES, CONSTRAINT_CHOICES
 import datetime
 
-"""
-displays checkboxes for Client Search
-"""
 class ClientForm(forms.Form):
+    """
+    displays checkboxes for Client Search
+    """
     client_fields = forms.MultipleChoiceField(required=False,
     widget=forms.CheckboxSelectMultiple, choices=CLIENT_FIELD_CHOICES)
 
-"""
-displays chechboxes for Order Search
-"""        
 class OrderForm(forms.Form):
+    """
+    displays chechboxes for Order Search
+    """        
     order = forms.MultipleChoiceField(required=False,
     widget=forms.CheckboxSelectMultiple, choices=CLIENT_ORDER_CHOICES)
 
-"""
-displays checkboxes for Constraints
-"""
 class AddConstraints(forms.Form):
+    """
+    displays checkboxes for Constraints
+    """
     start_date = forms.DateField(required=False, initial='2014-01-01')
     end_date = forms.DateField(required=False, initial= datetime.date.today())
     additional_constraints = forms.MultipleChoiceField(required=False,
